fb84bb68971f4e3bdf644ff75e9e1b1909a5ab31
EmiiEu4n <gaspareuan155@gmail.com>
2024-05-14 17:36:09 -0600

    Corrección github

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/EmiiEu4n/proyecto-gif-equipoDinamita-creando.git'
+  base: '/proyecto-gif-equipoDinamita-creando/'
 })
