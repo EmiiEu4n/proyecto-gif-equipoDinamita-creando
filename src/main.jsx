aa5fbecfe86a54799d5b0f82dbb98a697a830eb0
EmiiEu4n <gaspareuan155@gmail.com>
2024-05-14 18:05:20 -0600

    git

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 // importamos la biblioteca reactDom, que proporciona metodos especificos del doom para react
 import ReactDOM from 'react-dom/client'
 //importamos un nuevo componente llamado ProyectoGif desde otro archivo en el mismo directorio
-import { ProyectoGif } from './ProyectoGif.jsx'
+import { ProyectoGif } from './ProyectoGif'
 
 import './styles.css'
 
